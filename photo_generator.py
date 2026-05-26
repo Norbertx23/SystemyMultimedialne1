fdb876fb6eb2b5bd8511311e16dae205e8f4adb9
Norbertx23 <norbertx23@gmail.com>
2026-05-26 16:33:27 +0200

    color + binary

--- photo_generator.py
+++ photo_generator.py
@@ -1,16 +1,33 @@
 import requests
+from PIL import Image
+import io
 
 width = 512
 height = 512
 save_path = "."
+amount = 3
 
-for i in range(3):
+save_as_binary = False
+
+for i in range(amount):
     url = f"https://picsum.photos/{width}/{height}"
     response = requests.get(url)
 
     if response.status_code == 200:
-        with open(f"{save_path}/image{i}.jpg", "wb") as file:
-            file.write(response.content)
-        print(f"Zdjęcie {width}x{height} zostało pomyślnie pobrane!")
+        if save_as_binary:
+            image_data = io.BytesIO(response.content)
+            img = Image.open(image_data)
+            binary_img = img.convert('1')
+
+            file_name = f"{save_path}/image_binary_{i}.png"
+            binary_img.save(file_name)
+            print(f"Binarny obraz {width}x{height} zapisany jako {file_name}!")
+
+        else:
+            file_name = f"{save_path}/image_normal_{i}.jpg"
+            with open(file_name, "wb") as file:
+                file.write(response.content)
+            print(f"Normalne zdjęcie {width}x{height} zapisane jako {file_name}!")
+
     else:
-        print("Błąd podczas pobierania zdjęcia.")
+        print(f"Błąd podczas pobierania zdjęcia {i}. Kod błędu: {response.status_code}")
